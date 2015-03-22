f4fceee789d68c67f97ce652c38c66080694a150
Anatoly Tarasenko <fm.ksbw@gmail.com>
2015-03-22 18:26:26 +0000

    Update break.py

--- break.py
+++ break.py
@@ -5,5 +5,5 @@ num = 0
 print "This program started on "+time.ctime()
 while num < 3:
     time.sleep(1) # here enter time between breaks. In seconds. 1h = 60*60
-    webbrowser.open("http://at02.ru")
+    webbrowser.open("http://at02.ru/category/code/")
     num += 1
