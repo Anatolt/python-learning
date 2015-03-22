0f46a7d2a5a790bf232f749f9e5031d4672c1939
Anatoly Tarasenko <fm.ksbw@gmail.com>
2015-03-23 00:27:59 +0600

    Update break.py

--- break.py
+++ break.py
@@ -3,7 +3,7 @@ import webbrowser
 
 num = 0
 print "This program started on "+time.ctime()
-while num < 3:
+while num < 3: # how many breaks you want
     time.sleep(1) # here enter time between breaks. In seconds. 1h = 60*60
-    webbrowser.open("http://at02.ru/category/code/")
+    webbrowser.open("http://at02.ru/category/code/") # what do you want to open
     num += 1
