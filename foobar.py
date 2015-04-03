b6b9d4301b6c37291dbc1cc093729b899a36b405
Anatoly Tarasenko <fm.ksbw@gmail.com>
2015-04-03 14:17:59 -0800

    It works!

--- foobar.py
+++ foobar.py
@@ -1,13 +1,12 @@
 def foobar(n):
     for i in range(n):
-        print i
-        if i % 3 == 0:
+        if i % 15 == 0:
             print "foo"
         elif i % 5 == 0:
             print "bar"
-        elif i % 15 == 0:
-            return "foobar"
+        elif i % 3 == 0:
+            print "foobar"
         else:
-            return i
+            print i
 
 print foobar(20)
